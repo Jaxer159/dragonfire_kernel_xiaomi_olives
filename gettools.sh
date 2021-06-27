f916dc269fe7135cbe5d27ca09b2ee7e5efcadb6
Nazar Kovalenko <85736144+Jaxer159@users.noreply.github.com>
2021-06-27 21:17:22 +0300

    Update gettools.sh

--- gettools.sh
+++ gettools.sh
@@ -3,22 +3,14 @@
 # Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
 # Copyright (C) 2018 Rama Bondan Prakoso (rama982)
 # Android Kernel Build Script
-
-# Clone AnyKernel3
 git clone https://github.com/jaxer159/AnyKernel3 -b dragonfire_olives_iw
 mv AnyKernel3/ IW/
 git clone https://github.com/jaxer159/AnyKernel3 -b dragonfire_olives
-
-#Download Clang
 if [ ! -d $HOME/toolchains/proton-clang ]; then
 mkdir $HOME/toolchains
 git clone https://github.com/kdrag0n/proton-clang $HOME/toolchains/proton-clang --depth=1
 fi
-
-# Download libufdt
-if [ ! -d libufdt ]; then
-    wget https://android.googlesource.com/platform/system/libufdt/+archive/refs/tags/android-10.0.0_r45/utils.tar.gz
-    mkdir -p libufdt
-    tar xvzf utils.tar.gz -C libufdt
-    rm utils.tar.gz
-fi
+wget https://android.googlesource.com/platform/system/libufdt/+archive/refs/tags/android-10.0.0_r45/utils.tar.gz
+mkdir -p libufdt
+tar xvzf utils.tar.gz -C libufdt
+rm utils.tar.gz
