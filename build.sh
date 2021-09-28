c24d5d78eb441b5aedb8c5e6bdc743e874ee2ca4
Nazar Kovalenko <85736144+Jaxer159@users.noreply.github.com>
2021-09-28 21:14:30 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@
 # Copyright (C) 2018 Rama Bondan Prakoso (rama982)
 # Android Kernel Build Script
 
+#Package
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+
 # Clone AnyKernel3
 if [ ! -d AnyKernel3 ]; then
 git clone https://github.com/Jaxer159/AnyKernel3 -b dragonfire_olives AnyKernel3
